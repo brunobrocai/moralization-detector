168b39d8cc901933abf9212223979aec45413827
Bruno Brocai <bruno-brocai@gmx.de>
2024-07-25 16:08:45 +0200

    Remove code referring to TPUs

--- util.py
+++ util.py
@@ -1,14 +1,9 @@
 import torch
-import torch_xla
-import torch_xla.core.xla_model as xm
 
 
 def get_device():
-    # Check if TPU is available
-    if 'xla' in str(xm.xla_device()):
-        device = xm.xla_device()
-        print('TPU detected. Using XLA device:', device)
-    elif torch.cuda.is_available():
+
+    if torch.cuda.is_available():
         device = torch.device("cuda")
         print(
             'No TPU detected. There are %d GPU(s) available.' % torch.cuda.device_count()
