f29f1f44b1c8c77d745e527755565861c94546f1
Bruno Brocai <bruno-brocai@gmx.de>
2024-07-26 13:14:05 +0200

    Add method to represent instances as list of dicts

--- util.py
+++ util.py
@@ -6,10 +6,17 @@ def get_device():
     if torch.cuda.is_available():
         device = torch.device("cuda")
         print(
-            'No TPU detected. There are %d GPU(s) available.' % torch.cuda.device_count()
+            'No TPU detected. '
+            f'There are {torch.cuda.device_count()} GPU(s) available.'
         )
         print('Using GPU:', torch.cuda.get_device_name(0))
     else:
         print('No TPU or GPU detected, using the CPU instead.')
         device = torch.device("cpu")
     return device
+
+
+def results_to_dict(results):
+    return [
+        result.to_dict() for result in results
+    ]
